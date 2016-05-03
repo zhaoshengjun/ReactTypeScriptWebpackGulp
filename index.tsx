e3d60aa2072909bb0e3c91add4a0162a9d531437
Sam Zhao <zhao.shengjun@gmail.com>
2016-05-03 15:52:06 +1000

    Show higher order component

--- index.tsx
+++ index.tsx
@@ -1,28 +1,68 @@
 import * as React from 'react';
 import {Component, PropTypes} from 'react';
 
-import {render} from 'react-dom';
+import * as ReactDOM from 'react-dom';
 
-class Greeter extends Component<any, any> {
-  static propTypes;
-  static defaultProps;
+let Mixin = InnerComponent => class extends Component<any, any> {
+  constructor(props) {
+    super(props);
+    console.log("constructor");  
+    this.state = {
+      val: 0
+    };
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.setState({
+      val: this.state.val + 1
+    });
+  }
+
+  componentWillMount() {
+    console.log("component will mount");
+  }  
+
+  componentDidMount() {
+    console.log("component did mount");
+    
+  }
+
   render() {
-    return (
-      <h1>{this.props.salutation}</h1>
-    )
+    return <InnerComponent 
+      update={this.update}
+      state={this.state}
+      props={this.props}
+      />
   }
 }
 
-Greeter.propTypes = {
-  // salutation: PropTypes.string.isRequired
-  salutation: PropTypes.string
+const Button = (props) => {
+  return <button onClick={props.update}> {props.props.txt} - {props.state.val}</button>;
 }
 
-Greeter.defaultProps = {
-  salutation: 'Hello World'
+const Label = (props) => {
+  return <label onMouseMove={props.update}>{props.props.txt} - {props.state.val} </label>
 }
 
-render(
-  <Greeter salutation="hi hello nihao" />,
+let ButtonMixed = Mixin(Button)
+let LabelMixed = Mixin(Label)
+
+class App extends Component<any, any> { 
+  render() {
+    return (
+      <div>
+        <ButtonMixed
+          txt="Button"
+          />
+        <LabelMixed
+          txt="Label"
+          />
+      </div>
+    );
+  }
+}
+ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
