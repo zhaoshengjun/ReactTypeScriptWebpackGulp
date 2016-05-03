7df74853d5524dbbcf45f9e605250278778a57ed
Sam Zhao <zhao.shengjun@gmail.com>
2016-05-03 15:25:43 +1000

    Show props lifecycle events

--- index.tsx
+++ index.tsx
@@ -1,28 +1,50 @@
 import * as React from 'react';
 import {Component, PropTypes} from 'react';
 
-import {render} from 'react-dom';
+import * as ReactDOM from 'react-dom';
 
-class Greeter extends Component<any, any> {
-  static propTypes;
+class App extends Component<any, any> {
   static defaultProps;
+  constructor(props) {
+    super(props);   
+    this.state = {
+      increasing: false
+    };
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    ReactDOM.render(<App val={this.props.val + 1} />, document.getElementById('root'));
+  }
+
+  componentWillReceiveProps(nextProps) {  
+    this.setState({
+      increasing: nextProps.val > this.props.val
+    })
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.val % 5 === 0;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevProps: ', prevProps);
+    
+  }
   render() {
+    console.log(this.state.increasing);
+    
     return (
-      <h1>{this.props.salutation}</h1>
-    )
+      <button onClick={this.update}>{this.props.val}</button>
+    );
   }
 }
 
-Greeter.propTypes = {
-  // salutation: PropTypes.string.isRequired
-  salutation: PropTypes.string
+App.defaultProps = {
+  val: 0
 }
 
-Greeter.defaultProps = {
-  salutation: 'Hello World'
-}
-
-render(
-  <Greeter salutation="hi hello nihao" />,
+ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
