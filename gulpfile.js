23019eee011063f1e7fc978bfd6f42f861cae428
Sam Zhao <zhao.shengjun@gmail.com>
2016-05-06 14:52:52 +1000

    Update gulpfile to enable monitoring html and css files.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var connect = require('gulp-connect');
 var webpack = require('webpack-stream');
 var webpackConfig = require('./webpack.config.js');
 
+var htmlFiles = 'index.html';
+var cssFiles = 'styles/*.css';
+
+
 gulp.task('server', function () {
   connect.server({
     livereload: true,
@@ -10,11 +14,27 @@ gulp.task('server', function () {
   })
 });
 
+
+gulp.task('html', function () {
+  gulp.src(htmlFiles)
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+  gulp.src(cssFiles)
+    .pipe(connect.reload());
+});
+
 gulp.task('webpack', function () {
-  gulp.src('index.js')
+  gulp.src('src/index.js')
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest('dist/'))
     .pipe(connect.reload());
 });
 
-gulp.task('default', ['server', 'webpack']);
+gulp.task('watch', function () {
+  gulp.watch(htmlFiles, ['html']);
+  gulp.watch(cssFiles, ['css']);
+});
+
+gulp.task('default', ['server','watch', 'webpack']);
